3ec8d73ee7fda060a9ae7f0bfcce5a975ba414e4
sLav9n4ik <slava.tresnevv@gmail.com>
2018-10-18 09:54:16 +0300

    Composite + Iterator + 5 UnitTests

--- src/main/java/ru/sbt/mipt/oop/Application.java
+++ src/main/java/ru/sbt/mipt/oop/Application.java
@@ -1,22 +1,42 @@
 package ru.sbt.mipt.oop;
 
-import ru.sbt.mipt.oop.dataload.FileSmartHomeLoader;
-import ru.sbt.mipt.oop.dataload.SmartHomeLoader;
-import ru.sbt.mipt.oop.processing.EventProcessing;
-
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+
+import static ru.sbt.mipt.oop.SensorEventType.*;
 
 public class Application {
 
     private static SmartHomeLoader smartHomeLoader = new FileSmartHomeLoader();
 
-    public static void main(String... args) throws IOException {
-        SmartHome smartHome = smartHomeLoader.loadSmartHome();
-        EventProcessing.runEventsCycle(smartHome);
-    }
-
     public static void setSmartHomeLoader(SmartHomeLoader smartHomeLoader) {
         Application.smartHomeLoader = smartHomeLoader;
+    }
+
+    public static void main(String... args) throws IOException {
+        SmartHome smartHome = smartHomeLoader.loadSmartHome();
+        runEventsCycle(smartHome);
+    }
+
+    private static void runEventsCycle(SmartHome smartHome) {
+        SensorEvent event = RandomSensorEventProvider.getNextSensorEvent();
+        Collection<EventProcessor> eventProcessors = configureEventProcessors();
+        while (event != null) {
+            System.out.println("Got event: " + event);
+            for (EventProcessor eventProcessor : eventProcessors) {
+                eventProcessor.processEvent(smartHome, event);
+            }
+            event = RandomSensorEventProvider.getNextSensorEvent();
+        }
+    }
+
+    private static Collection<EventProcessor> configureEventProcessors() {
+        Collection<EventProcessor> eventProcessors = new ArrayList<>();
+        eventProcessors.add(new LightsEventProcessor());
+        eventProcessors.add(new DoorEventProcessor());
+        eventProcessors.add(new HallDoorEventProcessor());
+        return eventProcessors;
     }
 
 }
--- src/main/java/ru/sbt/mipt/oop/dataload/SmartHomeLoader.java
+++
@@ -1,9 +0,0 @@
-package ru.sbt.mipt.oop.dataload;
-
-import ru.sbt.mipt.oop.SmartHome;
-
-import java.io.IOException;
-
-public interface SmartHomeLoader {
-    SmartHome loadSmartHome() throws IOException;
-}
