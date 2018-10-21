55750e9bec0cf6cb2047e5592d67b9ba86032b26
slav9n4ik <slava.tresnevv@gmail.com>
2018-10-21 09:43:55 +0300

    Observer refactoring

--- src/main/java/ru/sbt/mipt/oop/Application.java
+++ src/main/java/ru/sbt/mipt/oop/Application.java
@@ -1,8 +1,11 @@
 package ru.sbt.mipt.oop;
 
-import ru.sbt.mipt.oop.observer.DoorEventHandler;
+import ru.sbt.mipt.oop.eventprocessors.DoorEventProcessor;
+import ru.sbt.mipt.oop.eventprocessors.HallDoorEventProcessor;
+import ru.sbt.mipt.oop.eventprocessors.LightsEventProcessor;
+import ru.sbt.mipt.oop.observer.EventManager;
 import ru.sbt.mipt.oop.observer.HomeEventsObserver;
-import ru.sbt.mipt.oop.observer.LightEventHandler;
+import ru.sbt.mipt.oop.sensors.RandomSensorEventProvider;
 import ru.sbt.mipt.oop.sensors.SensorEventType;
 
 import java.io.IOException;
@@ -10,10 +13,12 @@ import java.io.IOException;
 public class Application {
 
     private SmartHomeLoader smartHomeLoader = new FileSmartHomeLoader();
+    private HomeEventsObserver observer;
+    private EventManager listenersManager =  new EventManager(SensorEventType.values());
 
-//    public void setSmartHomeLoader(SmartHomeLoader smartHomeLoader) {
-//        Application.smartHomeLoader = smartHomeLoader;
-//    }
+    public void setSmartHomeLoader(SmartHomeLoader smartHomeLoader) {
+        this.smartHomeLoader = smartHomeLoader;
+    }
 
     public static void main(String... args) throws IOException {
         new Application().startApplication();
@@ -21,15 +26,21 @@ public class Application {
 
     private void startApplication() throws IOException {
         SmartHome smartHome = smartHomeLoader.loadSmartHome();
-
-        HomeEventsObserver observer = new HomeEventsObserver();
-        observer.events.subscribe(SensorEventType.DOOR_OPEN, new DoorEventHandler());
-        observer.events.subscribe(SensorEventType.DOOR_CLOSED, new DoorEventHandler());
-        observer.events.subscribe(SensorEventType.LIGHT_ON, new LightEventHandler());
-        observer.events.subscribe(SensorEventType.LIGHT_OFF, new LightEventHandler());
-
-        //НЕЗАБУДЬ ПРО ТРЕТИЙ !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        makeNotifications();
         observer.runEventsCycle(smartHome);
+    }
+
+    private void makeNotifications() {
+        listenersManager.subscribe(SensorEventType.DOOR_OPEN, new DoorEventProcessor());
+        listenersManager.subscribe(SensorEventType.DOOR_CLOSED, new DoorEventProcessor());
+        listenersManager.subscribe(SensorEventType.LIGHT_ON, new LightsEventProcessor());
+        listenersManager.subscribe(SensorEventType.LIGHT_OFF, new LightsEventProcessor());
+        listenersManager.subscribe(SensorEventType.DOOR_CLOSED, new HallDoorEventProcessor());
+
+        observer = new HomeEventsObserver(
+                new RandomSensorEventProvider(),
+                listenersManager
+        );
     }
 
 }
--- src/main/java/ru/sbt/mipt/oop/observer/EventHandler.java
+++
@@ -1,8 +0,0 @@
-package ru.sbt.mipt.oop.observer;
-
-import ru.sbt.mipt.oop.SmartHome;
-import ru.sbt.mipt.oop.sensors.SensorEvent;
-
-public interface EventHandler {
-    void update(SensorEvent event, SmartHome smartHome);
-}
