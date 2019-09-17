adcce56bc85d696a79f6c83f82715e230b2fc0e4
slav9n4ik <slava.tresnevv@gmail.com>
2019-09-17 20:17:19 +0300

    Кэширование при старте + логирование

--- src/main/java/ru/sberbank/optdemo1/Opdemo1Controller.java
+++ src/main/java/ru/sberbank/optdemo1/Opdemo1Controller.java
@@ -1,59 +1,32 @@
 package ru.sberbank.optdemo1;
 
-import org.asynchttpclient.AsyncHttpClient;
-import org.asynchttpclient.Response;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.*;
-import java.util.concurrent.ExecutionException;
+import java.util.List;
 
 @RestController
 @RequestMapping("/demo1")
 public class Opdemo1Controller {
 
-	private static final String URL = "http://export.rbc.ru/free/selt.0/free.fcgi?period=DAILY&tickers=USD000000TOD&separator=TAB&data_format=BROWSER";
-	private static Map<Integer, List<Quote>> quotesMap = new HashMap<>();
+	private final QuoteCaching quoteCaching;
+	private Logger log = LoggerFactory.getLogger(QuoteCaching.class);
+
+	@Autowired
+	public Opdemo1Controller(QuoteCaching quoteCaching) {
+		this.quoteCaching = quoteCaching;
+	}
 
 	@RequestMapping("/quotes")
 	public @ResponseBody
-	List<Quote> quotes(@RequestParam("days") Integer days) throws ExecutionException, InterruptedException, ParseException {
-
-		if (!quotesMap.containsKey(days)) {
-			AsyncHttpClient client = AsyncHttpClientFactory.create(new AsyncHttpClientFactory.AsyncHttpClientConfig());
-			Response response = client.prepareGet(URL + "&lastdays=" + days).execute().get();
-			String body = response.getResponseBody();
-
-			String[] lines = body.split("\n");
-			List<Quote> quotes = new ArrayList<>();
-
-			for (String s : lines) {
-				String[] line = s.split("\t");
-				Double high = Double.parseDouble(line[3]);
-
-				//Сделай Builder
-				Quote quote = new Quote(line[0],
-						new SimpleDateFormat("yyyy-MM-dd").parse(line[1]),
-						Double.parseDouble(line[2]),
-						Double.parseDouble(line[3]),
-						Double.parseDouble(line[4]),
-						Double.parseDouble(line[5]),
-						Long.parseLong(line[6]),
-						Double.parseDouble(line[7]));
-				quote.setMaxInMonth(high);
-				quote.setMaxInYear(high);
-
-				quotes.add(quote);
-			}
-
-			quotesMap.put(days, quotes);
-		}
-
-		return quotesMap.get(days);
+	List<Quote> quotes(@RequestParam("days") Integer days){
+		log.info("Request /demo1/quotes?days=  SUCCESS");
+		return quoteCaching.quotesMap.get(days);
 	}
 
 }
---
+++ src/main/java/ru/sberbank/optdemo1/QuoteCaching.java
@@ -0,0 +1,92 @@
+package ru.sberbank.optdemo1;
+
+import org.asynchttpclient.AsyncHttpClient;
+import org.asynchttpclient.Response;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
+
+@Component
+public class QuoteCaching implements ApplicationListener<ApplicationReadyEvent> {
+
+    private Logger log = LoggerFactory.getLogger(QuoteCaching.class);
+
+    public static Map<Integer, List<Quote>> quotesMap = new HashMap<>();
+    private static final String URL = "http://export.rbc.ru/free/selt.0/free.fcgi?period=DAILY&tickers=USD000000TOD&separator=TAB&data_format=BROWSER";
+
+    @Override
+    public void onApplicationEvent(final ApplicationReadyEvent event) {
+        log.info("Start caching");
+        AsyncHttpClient client = AsyncHttpClientFactory.create(new AsyncHttpClientFactory.AsyncHttpClientConfig());
+
+        for (int i = 1; i < 101; i++) {
+            System.out.println(i);
+            Response response = null;
+            try {
+                response = client.prepareGet(URL + "&lastdays=" + i).execute().get();
+            } catch (InterruptedException | ExecutionException e) {
+                e.printStackTrace();
+            }
+            String body = response.getResponseBody();
+
+            String[] lines = body.split("\n");
+            List<Quote> quotes = new ArrayList<>();
+
+            for (String s : lines) {
+                String[] line = s.split("\t");
+                Double high = Double.parseDouble(line[3]);
+
+                //Сделай Builder
+                Quote quote = null;
+                try {
+                    quote = new Quote(line[0],
+                            new SimpleDateFormat("yyyy-MM-dd").parse(line[1]),
+                            Double.parseDouble(line[2]),
+                            Double.parseDouble(line[3]),
+                            Double.parseDouble(line[4]),
+                            Double.parseDouble(line[5]),
+                            Long.parseLong(line[6]),
+                            Double.parseDouble(line[7]));
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+                quote.setMaxInMonth(high);
+                quote.setMaxInYear(high);
+
+                quotes.add(quote);
+            }
+
+            quotesMap.put(i, quotes);
+        }
+
+        log.info("Caching end");
+
+        quotesMap.forEach((k,v) -> {
+            log.info("key: " + k + " " + v.toString());
+        });
+
+        requestYourSelf(client, 100);
+        return;
+    }
+
+    private void requestYourSelf(AsyncHttpClient client, int times) {
+        for (int i = 0; i < times; i++) {
+            log.info("Request: " + i);
+            try {
+                client.prepareGet("http://localhost:8080/demo1/quotes?days=30").execute().get();
+            } catch (InterruptedException | ExecutionException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
